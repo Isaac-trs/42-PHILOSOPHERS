d26bb3b9e476436f67786da658d8b1f8245ac96f
istripol <istripol@student.42.fr>
2025-05-24 12:31:02 +0200

    moves dir

---
+++ philosophers/philosophers.h
@@ -0,0 +1,130 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: istripol <istripol@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 14:39:57 by istripol          #+#    #+#             */
+/*   Updated: 2025/05/24 12:21:16 by istripol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include <sys/time.h>
+
+# define RED "\033[0;91m"
+# define GREEN "\033[0;92m"
+# define YELLOW "\033[0;93m"
+# define ORANGE "\033[38;5;208m"
+# define CYAN "\033[0;96m"
+# define BOLD "\033[1m"
+# define RESET "\033[0m"
+
+// STATES
+# define ERRPHILO 1
+# define ERRFORKS 2
+# define ERREXIT  3
+# define ERRARGS  4
+
+typedef unsigned int	t_bool;
+
+typedef struct s_philo
+{
+	pthread_t			thread;
+	pthread_mutex_t		*write_lock;
+	pthread_mutex_t		*dead_lock;
+	pthread_mutex_t		meal_mutex;
+
+	struct s_program	*program;
+	int					id;
+	long long			last_meal; //(microseconds)
+	unsigned int		nb_meals;
+	unsigned int		nb_think;
+
+	struct s_fork		*left_fork;
+	struct s_fork		*right_fork;
+
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					time_to_die;
+
+	t_bool				thinking;
+	t_bool				eating;
+	t_bool				dead;
+
+	// First philo's right fork is last's left fork
+	// Last philo's left fork is first's right fork
+}	t_philo;
+
+typedef struct s_fork
+{
+	pthread_mutex_t	mutex;
+	unsigned int	id;
+	t_bool			in_use;
+	//t_philo			*left_philo;
+	//t_philo			*right_philo;
+}	t_fork;
+
+typedef struct s_program
+{
+	pthread_mutex_t	write_lock;	// printf - thread-safe
+	pthread_mutex_t	dead_lock;	// if a philo dies lock
+	t_bool			foo_died;	// if a philo dies var
+	t_philo			*philos;// array
+	t_fork			*forks;		//	array
+	pthread_t		monitor;
+	long long		started; //(microseconds)
+	int				nb_philos;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				time_to_die;
+	int				nb_meals;
+}	t_program;
+
+typedef enum e_flag
+{
+	eat,
+	sleeep,
+	think,
+	forking,
+	start,
+	threadd,
+	all,
+	//fork,
+	null,
+	philosophers
+}	t_flag;
+
+// functions.c
+void		print_timestamp(char *message, t_philo *philo);
+int			ft_atoi(const char *nptr);
+t_bool		is_number(char *str);
+void		clean_exit(t_program *program, t_flag flag, int thread);
+long long	get_time_ms(void);
+void		ft_usleep(long ms);
+void		ft_exit(t_program *program, int flag);
+// debug.c
+void		print_philosophers(t_program *programs);
+void		print_success(char *word);
+
+// philos.c
+void		start_philos(t_program *program);
+t_philo		*init_philos(t_program *program);
+t_bool		check_and_init(char **args, t_program *program);
+t_fork		*init_forks(t_program *program, t_philo *philos);
+
+// routine.c
+void		*start_routine(void *philo);
+
+// monitor.c
+t_bool		check_death_lock(t_program *program);
+void		*monitor_thread(void *arg);
+
+#endif
---
+++ philosophers/routine.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: istripol <istripol@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 14:29:36 by istripol          #+#    #+#             */
+/*   Updated: 2025/05/24 12:23:01 by istripol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+static void	lock_forks(void *philo)
+{
+	if (((t_philo *)philo)->id % 2 == 0)
+	{
+		pthread_mutex_lock(&((t_philo *)philo)->right_fork->mutex);
+		print_timestamp(ORANGE" has taken a fork\n"RESET, ((t_philo *)philo));
+		pthread_mutex_lock(&((t_philo *)philo)->left_fork->mutex);
+		print_timestamp(ORANGE" has taken a fork\n"RESET, ((t_philo *)philo));
+	}
+	else
+	{
+		pthread_mutex_lock(&((t_philo *)philo)->left_fork->mutex);
+		print_timestamp(ORANGE" has taken a fork \n"RESET, ((t_philo *)philo));
+		pthread_mutex_lock(&((t_philo *)philo)->right_fork->mutex);
+		print_timestamp(ORANGE" has taken a fork \n"RESET, ((t_philo *)philo));
+	}
+}
+
+static void	unlock_forks(void *philo)
+{
+	if (((t_philo *)philo)->id % 2 == 0)
+	{
+		pthread_mutex_unlock(&((t_philo *)philo)->right_fork->mutex);
+		pthread_mutex_unlock(&((t_philo *)philo)->left_fork->mutex);
+	}
+	else
+	{
+		pthread_mutex_unlock(&((t_philo *)philo)->left_fork->mutex);
+		pthread_mutex_unlock(&((t_philo *)philo)->right_fork->mutex);
+	}
+}
+
+static void	*eating(void *phil)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)phil;
+	pthread_mutex_lock(& philo->meal_mutex);
+	philo->last_meal = get_time_ms();
+	philo->nb_meals++;
+	pthread_mutex_unlock(& philo->meal_mutex);
+	lock_forks(philo);
+	print_timestamp(YELLOW" is eating\n"RESET, philo);
+	ft_usleep(philo->time_to_eat);
+	unlock_forks(philo);
+	print_timestamp(GREEN" finished eating\n"RESET, philo);
+	print_timestamp(YELLOW" is thinking\n"RESET, ((t_philo *)philo));
+	return (NULL);
+}
+
+static void	*sleeping(void *phil)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)phil;
+	print_timestamp(YELLOW" is sleeping\n"RESET, philo);
+	ft_usleep(philo->time_to_sleep);
+	print_timestamp(GREEN" finished sleeping\n"RESET, philo);
+	print_timestamp(YELLOW" is thinking\n"RESET, ((t_philo *)philo));
+	return (NULL);
+}
+
+void	*start_routine(void *philo)
+{
+	if (((t_philo *)philo)->id % 2 != 0)
+		ft_usleep(1);
+	pthread_mutex_lock(&((t_philo *)philo)->meal_mutex);
+	((t_philo *)philo)->last_meal = get_time_ms();
+	pthread_mutex_unlock(&((t_philo *)philo)->meal_mutex);
+	while (1)
+	{
+		if (((t_philo *)philo)->id % 2 != 0)
+		{
+			if (check_death_lock(((t_philo *)philo)->program) == 1)
+				break ;
+			sleeping((t_philo *)philo);
+			if (check_death_lock(((t_philo *)philo)->program) == 1)
+				break ;
+			eating((t_philo *)philo);
+		}
+		else
+		{
+			eating((t_philo *)philo);
+			if (check_death_lock(((t_philo *)philo)->program) == 1)
+				break ;
+			sleeping((t_philo *)philo);
+			if (check_death_lock(((t_philo *)philo)->program) == 1)
+				break ;
+		}
+	}
+	return ((int *)1);
+}
